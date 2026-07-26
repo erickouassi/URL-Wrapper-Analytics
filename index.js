fbcd7f237bee151fe5c71eeae7e70fd2857fd336
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-07-26 12:51:24 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js (Root entrypoint)
-export default function handler(req, res) {
-  res.status(200).json({
-    status: 'online',
-    service: 'Url-Wrapper Redirect Engine',
-    documentation: '/stats'
-  });
-}
