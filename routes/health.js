2c1bdcff75ba1e72e504add6769666785acd3676
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-07-26 01:14:05 -0400

    Delete routes directory

--- routes/health.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-  app.get('/health', (req, res) => {
-    res.json({ status: "ok", timestamp: Date.now() });
-  });
-};
--- routes/statsPage.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = function(app) {
-  app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'stats.html'));
-  });
-};
